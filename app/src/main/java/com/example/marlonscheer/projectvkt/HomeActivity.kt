e92aff93dc89e592788b7bdc228de70eb409d1f4
R3DX03 <marlon.scheer@t-online.de>
2019-05-29 17:32:46 +0200

    Commented

--- app/src/main/java/com/example/marlonscheer/projectvkt/HomeActivity.kt
+++ app/src/main/java/com/example/marlonscheer/projectvkt/HomeActivity.kt
@@ -12,25 +12,31 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-
-
+        //Load WebView
         webView.loadUrl("https://github.com")
+        //Confirm Connection
         Async().execute("Connection success")
-
+        //Close Socket
         buttonClose.setOnClickListener {
             finish()
         }
 
         arrup.setOnTouchListener(OnTouchListener { view, event ->
+            //Checks State of arrow
             if (event.action == android.view.MotionEvent.ACTION_DOWN) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "21 ", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrup.setImageDrawable(getDrawable(R.drawable.arrowupgreen))
+                //Send Code to AsyncTask
                 Async().execute("21")
 
-
             } else if (event.action == android.view.MotionEvent.ACTION_UP) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "22", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrup.setImageDrawable(getDrawable(R.drawable.arrowup))
+                //Send Code to AsyncTask
                 Async().execute("22")
 
             }
@@ -39,15 +45,21 @@ class HomeActivity : AppCompatActivity() {
 
 
         arrdown.setOnTouchListener(OnTouchListener { view, event ->
+            //Checks State of arrow
             if (event.action == android.view.MotionEvent.ACTION_DOWN) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "31", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrdown.setImageDrawable(getDrawable(R.drawable.arrowdowngreen))
+                //Send Code to AsyncTas
                 Async().execute("31")
 
-
             } else if (event.action == android.view.MotionEvent.ACTION_UP) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "32", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrdown.setImageDrawable(getDrawable(R.drawable.arrowdown))
+                //Send Code to AsyncTask
                 Async().execute("32")
 
 
@@ -56,16 +68,21 @@ class HomeActivity : AppCompatActivity() {
         })
 
         arrleft.setOnTouchListener(OnTouchListener { view, event ->
+            //Checks State of arrow
             if (event.action == android.view.MotionEvent.ACTION_DOWN) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "41", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrleft.setImageDrawable(getDrawable(R.drawable.arrowleftgreen))
+                //Send Code to AsyncTask
                 Async().execute("41")
 
-
-
             } else if (event.action == android.view.MotionEvent.ACTION_UP) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "42", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrleft.setImageDrawable(getDrawable(R.drawable.arrowleft))
+                //Send Code to AsyncTask
                 Async().execute("42")
 
             }
@@ -73,19 +90,22 @@ class HomeActivity : AppCompatActivity() {
         })
 
         arrright.setOnTouchListener(OnTouchListener { view, event ->
+            //Checks State of arrow
             if (event.action == android.view.MotionEvent.ACTION_DOWN) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "51", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrright.setImageDrawable(getDrawable(R.drawable.arrowrightgreen))
+                //Send Code to AsyncTask
                 Async().execute("51")
 
-
-
             } else if (event.action == android.view.MotionEvent.ACTION_UP) {
+                //Show Code in Snackbar
                 Snackbar.make(homeView, "52", Snackbar.LENGTH_SHORT).show()
+                //Change Icon
                 arrright.setImageDrawable(getDrawable(R.drawable.arrowright))
+                //Send Code to AsyncTask
                 Async().execute("52")
-
-
             }
             true
         })
